5c32d773e2e63478386719ad322211edbbe05146
iammkaifkhan <iammkaifkhan@gmail.com>
2025-06-27 17:07:22 +0530

    Added routing

--- src/components/Pokedex/Pokedex.jsx
+++ src/components/Pokedex/Pokedex.jsx
@@ -1,10 +1,12 @@
+import PokemonList from "../PokemonList/PokemonList";
 import Search from "../Search/Search"
 import './Pokedex.css'
 const Pokemon = () => {
   return (
-    <div className="pokemon-wrapper">
-      <h1 id="pokemon-heading">Pokemon</h1>
-      <Search/>
+    <div className="pokedex-wrapper">
+      
+      <Search />
+      <PokemonList />
     </div>
   );
 }
--- src/components/Pokemon/Pokemon.jsx
+++ src/components/Pokemon/Pokemon.jsx
@@ -1,9 +1,12 @@
+import { Link } from "react-router-dom"
 import "./Pokemon.css"
-const Pokemon = ({name, image}) => {
+const Pokemon = ({name, image, id}) => {
   return (
     <div className="pokemon">
+      <Link to={`/pokemon/${id}`}>
       <div className="pokemon-name">{name}</div>
       <div><img className="pokemon-image" src={image}/></div>
+    </Link>
     </div>
   )
 }
